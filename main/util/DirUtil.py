e3270008ee13dade24c3e8a078b569ecc3fe691a
shenranw <wsr.20021128@gmail.com>
2023-07-29 10:08:31 +0800

    Fix for bugs caused by local implementation, requests error pending

--- main/util/DirUtil.py
+++ main/util/DirUtil.py
@@ -1,7 +1,7 @@
 import os
 
 def get_root_dir():
-    return os.path.split(os.path.split(os.getcwd())[0])[0]
+    return os.getcwd()
 
 def get_image_dir():
     return f'{get_root_dir()}/resources/streetview_images'
