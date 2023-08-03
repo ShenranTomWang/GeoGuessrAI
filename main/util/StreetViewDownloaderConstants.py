a1b0ae86507c3a216b5bf4e2ed788ae972fbc56b
Kyrie <kai20021226@163.com>
2023-08-03 17:07:11 +0800

    message

--- main/util/StreetViewDownloaderConstants.py
+++ main/util/StreetViewDownloaderConstants.py
@@ -3,4 +3,34 @@ DEFAULT_DATASET_SIZE = 10000
 DEFAULT_IMAGE_SIZE = '640x640'
 DEFAULT_FOV = 120
 STREETVIEW_API_BASE_URL = 'https://maps.googleapis.com/maps/api/streetview'
-DEFAULT_PROMPTS = ['highways+in+Canada']
+DEFAULT_PROMPTS = [
+'highways+in+Canada','road+in+Canada','alleys+in+Canada',
+'highways+in+United+States','lanes+in+United+States','alleys+in+United+States','boulevards+in+United+States','trails+in+United+States','road+in+United+States',
+'highways+in+Cuba','lanes+in+Cuba','alley+in+Cuba',
+'highways+in+Mexico','lanes+in+Mexico','road+in+Mexico',
+'highways+in+Brizil''lanes+in+Brizil','alleys+in+Brizil',
+'road+in+Argentina','lanes+in+Argentina','street+in+Argentina',
+'road+in+Chile','lanes+in+Chile',
+'lanes+in+Peru','road+in+Peru','alleys+in+Peru',
+'alleys+in+Columbia','highways+in+Columbia','road+in+Columbia','lanes+in+Columbia',
+
+'alleys+in+New+Zealand','road+in+New+Zealand','highways+in+New+Zealand','lanes+in+New+Zealand',
+'lanes+in+Australia','highways+in+Australia','streets+in+Australia','roads+in+Australia',
+
+'roads+in+Madagascar','streets+in+Madagascar','lanes+in+Madagascar',
+'lanes+in+South+Africa','highways+in+South+Africa','streets+in+South+Africa',
+'streets+in+Namibia','lanes+in+Namibia',
+'roads+in+Angola','highways+in+Angola',
+'highways+in+Zambia','streets+in+Zambia','roads+in+Zambia',
+'highways+in+Mozambique','streets+in+Mozambique',
+'streets+in+Tanzania','highways+in+Tanzania','roads+in+Tanzania',
+'streets+in+Congo','lanes+in+Congo','highways+in+democratic+republic+of+congo','streets+in+democratic+republic+of+congo',
+'roads+in+Ethiopia','highways+in+Ethiopia',
+'streets+in+Uganda','highways+in+Uganda','roads+in+Uganda',
+'roads+in+Morocco','lanes+in+Morocco',
+'lanes+in+Egypt','streets+in+Egypt','roads+in+Egypt',
+
+
+
+
+]
