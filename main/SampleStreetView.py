d3d923db3abaf9ca0a9d4d73ef83dbac45d397db
shenranw <wsr.20021128@gmail.com>
2023-08-04 11:30:24 +0800

    Sampler now can pause sampling process and resume in another session Collected 20 images

--- main/SampleStreetView.py
+++ main/SampleStreetView.py
@@ -2,8 +2,8 @@ from util.StreetViewSampler import StreetViewSampler
 import util.StreetViewSamplerConstants as constants
 import util.DirUtil as DirUtil
 
-downloader = StreetViewSampler(100, constants.API_KEY)
+downloader = StreetViewSampler(10, constants.API_KEY)
 downloader.sample_coordinates()
 downloader.get_pano_ids_from_coordinates()
 downloader.download_street_view_images(DirUtil.get_image_dir())
-downloader.save_street_view_metadata(DirUtil.get_image_dir())
+downloader.save_sampler_status_metadata(DirUtil.get_image_dir())
--- main/util/StreetViewSampler.py
+++ main/util/StreetViewSampler.py
@@ -1,5 +1,4 @@
 import streetview
-from geopy.geocoders import Nominatim
 import os
 import json
 import requests
@@ -20,12 +19,11 @@ class StreetViewSampler:
         """
         Args:
             dataset_size (int): size of dataset.
-            api_key (string): google street view static api key.
-            image_size (string, optional): image size in format <int>x<int>, max 640x640. Defaults to constants.DEFAULT_IMAGE_SIZE.
-            fov (string, optional): image field of view. Defaults to constants.DEFAULT_FOV.
-            prompts (list<string>, optional): prompts to generate random coordinates. Defaults to constants.DEFAULT_PROMPTS.
+            api_key (str): google street view static api key.
+            image_size (str, optional): image size in format <int>x<int>, max 640x640. Defaults to constants.DEFAULT_IMAGE_SIZE.
+            fov (str, optional): image field of view. Defaults to constants.DEFAULT_FOV.
+            prompts (list<str>, optional): prompts to generate random coordinates. Defaults to constants.DEFAULT_PROMPTS.
         """
-        self.geolocator = Nominatim(user_agent='StreetViewDownloader')
         self.dataset_size = dataset_size
         self.api_key = api_key
         self.image_size = image_size
@@ -33,11 +31,12 @@ class StreetViewSampler:
         self.prompts = prompts
         self.coordinates = []
         self.pano_ids = []
+        self.prompt_descriptor = 0
 
     def sample_coordinates(self) -> None:
         """Generates random coordinates using Google's Places API, coordinates are stored in self.coordinates
         """
-        i = 0
+        i = self.prompt_descriptor
         while len(self.coordinates) < self.dataset_size and i < len(self.prompts):
             url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?query={self.prompts[i]}&key={self.api_key}'
             results = requests.get(url)
@@ -46,7 +45,8 @@ class StreetViewSampler:
                 results = results['results']
                 j = 0
                 while j < len(results) and len(self.coordinates) < self.dataset_size:
-                    self.coordinates.append({results[j]['geometry']['location']['lat'], results[j]['geometry']['location']['lng']})
+                    self.coordinates.append([results[j]['geometry']['location']['lat'], results[j]['geometry']['location']['lng']])
+                    j += 1
             else:
                 print(f'Prompt {self.prompts[i]} responded with error code {results.status_code}')
             i += 1
@@ -61,7 +61,7 @@ class StreetViewSampler:
             lat, lon = self.coordinates[i]
             panos = streetview.search_panoramas(lat, lon)
             if len(panos) > 0:
-                self.pano_ids.append(panos[0])
+                self.pano_ids.append(panos[0].pano_id)
             else:
                 self.coordinates.pop(i)
                 print(f'No panorama found at {lat}, {lon}')
@@ -72,11 +72,11 @@ class StreetViewSampler:
     def download_street_view_images(self, images_dir:str) -> None:
         """Downloads street view images. Always call self.generate_coordinates and self.get_pano_ids_from_coordinates prior to calling this method
         Args:
-            images_dir (string): directory to save images to
+            images_dir (str): directory to save images to
         """
         os.makedirs(images_dir, exist_ok=True)
         for i in range(0, self.dataset_size):
-            pano_id = self.pano_ids[i].pano_id
+            pano_id = self.pano_ids[i]
             lat, lon = self.coordinates[i]
             url = f'https://maps.googleapis.com/maps/api/streetview?size={self.image_size}&pano={pano_id}&key={self.api_key}'
             response = requests.get(url)
@@ -86,20 +86,44 @@ class StreetViewSampler:
                     pic.write(response.content)
         print("All images downloaded successfully.")
         
-    def save_street_view_metadata(self, metadata_dir:str, metadata_file_name:str=constants.DEFAULT_METADATA_FILE_NAME):
-        """Saves street view image metadata to a json file
+    def save_sampler_status_metadata(self, metadata_dir:str, metadata_file_name:str=constants.DEFAULT_METADATA_FILE_NAME) -> None:
+        """Saves sampler status metadata to a json file
 
         Args:
-            metadata_dir (string): directory to save metadata file to
-            metadata_file_name (string, optional): name of metadata file. Defaults to constants.DEFAULT_METADATA_FILE_NAME
+            metadata_dir (str): directory to save metadata file to
+            metadata_file_name (str, optional): name of metadata file. Defaults to constants.DEFAULT_METADATA_FILE_NAME
         """
         os.makedirs(metadata_dir, exist_ok=True)
-        jo = {
-            "coordinates": self.coordinates,
-            "pano_ids": self.pano_ids,
-            "image_size": self.image_size,
-            "fov": self.fov,
-            "dataset_size": self.dataset_size
+        with open(f'{metadata_dir}/{metadata_file_name}', 'w') as file:
+            file.write(json.dumps(self, default=StreetViewSampler.json_encoder))
+            
+    @staticmethod
+    def load_sampler_status_metadata(metadata_dir:str, dataset_size:int, api_key:str, metadata_file_name:str=constants.DEFAULT_METADATA_FILE_NAME) -> 'StreetViewSampler':
+        """Loads sampler status metadata from json file
+
+        Args:
+            metadata_dir (str): directory to metadata file
+            dataset_size (int): size of dataset desired
+            api_key (str): your Google API key
+            metadata_file_name (str, optional): name of metadata file. Defaults to constants.DEFAULT_METADATA_FILE_NAME.
+        """
+        jo = None
+        with open(f'{metadata_dir}/{metadata_file_name}') as file:
+            jo = json.load(file)
+        product = StreetViewSampler(dataset_size, api_key)
+        product.coordinates = jo['coordinates']
+        product.fov = jo['fov']
+        product.image_size = jo['image_size']
+        product.prompt_descriptor = jo['prompt_descriptor']
+        return product
+            
+    @staticmethod
+    def json_encoder(obj:'StreetViewSampler') -> object:
+        return {
+            "coordinates": obj.coordinates,
+            "pano_ids": obj.pano_ids,
+            "image_size": obj.image_size,
+            "fov": obj.fov,
+            "dataset_size": obj.dataset_size,
+            "prompt_descriptor": obj.prompt_descriptor + 1
         }
-        with open(f'{metadata_dir}/{metadata_file_name}', 'wb') as file:
-            file.write(json.dumps(jo))
