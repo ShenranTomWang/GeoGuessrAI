de73b1c9fa6506ca16d4e61368a8676855b1058c
shenranw <wsr.20021128@gmail.com>
2023-08-05 19:07:49 +0800

    Debug & logic fix. Sampler now samples wanted number of samples. 10 more samples are made

--- main/SampleStreetView.py
+++ main/SampleStreetView.py
@@ -6,11 +6,10 @@ import util.DirUtil as DirUtil
 sampler = None
 
 if os.path.exists(f'{DirUtil.get_image_dir()}/metadata.json'):
-    sampler = StreetViewSampler.load_sampler_status_metadata(DirUtil.get_image_dir(), 100, constants.API_KEY)
+    sampler = StreetViewSampler.load_sampler_status_metadata(DirUtil.get_image_dir(), 10, constants.API_KEY)
 else:
     sampler = StreetViewSampler(100, constants.API_KEY)
     
-sampler.sample_coordinates()
-sampler.get_pano_ids_from_coordinates()
+sampler.sample()
 sampler.download_panoramas(DirUtil.get_image_dir())
 sampler.save_sampler_status_metadata(DirUtil.get_image_dir())
--- main/util/StreetViewSampler.py
+++ main/util/StreetViewSampler.py
@@ -12,7 +12,7 @@ class StreetViewSampler:
     
     def __init__(
             self, 
-            dataset_size:int, 
+            sample_size:int, 
             api_key:str, 
             image_width:int=constants.DEFAULT_IMAGE_WIDTH,
             image_height:int=constants.DEFAULT_IMAGE_HEIGHT,
@@ -21,14 +21,14 @@ class StreetViewSampler:
         ) -> None:
         """
         Args:
-            dataset_size (int): dataset size of current sample session
+            sample_size (int): sample size of current sample session
             api_key (str): your Google API key
             image_width (int, optional): width of street view image. Defaults to constants.DEFAULT_IMAGE_WIDTH.
             image_height (int, optional): height of street view image. Defaults to constants.DEFAULT_IMAGE_HEIGHT.
             fov (str, optional): field of view. Defaults to constants.DEFAULT_FOV.
             prompts (list, optional): prompts to generate places. Defaults to constants.DEFAULT_PROMPTS.
         """
-        self.dataset_size = dataset_size
+        self.sample_size = sample_size
         self.api_key = api_key
         self.image_width = image_width
         self.image_height = image_height
@@ -40,44 +40,33 @@ class StreetViewSampler:
         self.prompt_descriptor = 0
         self.download_descriptor = 0
 
-    def sample_coordinates(self) -> None:
-        """Generates random coordinates using Google's Places API, coordinates are stored in self.coordinates
+    def sample(self) -> None:
+        """Generates random coordinates using Google's Places API and search for their corresponding panorama ids. Coordinates are stored in self.coordinates, pano ids are stored in self.pano_ids
         """
-        while len(self.coordinates) < self.dataset_size and self.prompt_descriptor < len(self.prompts):
+        self.download_descriptor = len(self.pano_ids)
+        while len(self.coordinates) < self.sample_size and self.prompt_descriptor < len(self.prompts):
             url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?query={self.prompts[self.prompt_descriptor]}&key={self.api_key}'
             results = requests.get(url)
             if (results.status_code == 200):
                 results = results.json()
                 results = results['results']
                 j = 0
-                while j < len(results) and len(self.coordinates) < self.dataset_size:
-                    self.coordinates.append([results[j]['geometry']['location']['lat'], results[j]['geometry']['location']['lng']])
+                while j < len(results) and len(self.coordinates) < self.sample_size:
+                    lat = results[j]['geometry']['location']['lat']
+                    lon = results[j]['geometry']['location']['lng']
+                    panos = streetview.search_panoramas(lat, lon)
+                    if len(panos) > 0:
+                        self.pano_ids.append(panos[0].pano_id)
+                        self.coordinates.append([lat, lon])
+                    else:
+                        print(f'No panorama found at {lat}, {lon}')
                     j += 1
             else:
                 print(f'Prompt {self.prompts[self.prompt_descriptor]} responded with error code {results.status_code}')
             self.prompt_descriptor += 1
-        if (len(self.coordinates) < self.dataset_size):
+        if (len(self.coordinates) < self.sample_size):
             print(f'Sampled {len(self.coordinates)} samples due to insufficient resources. Please add more prompts or check validity of requests')
-            self.dataset_size = len(self.coordinates)
-    
-    def get_pano_ids_from_coordinates(self) -> None:
-        """Gets panorama ids based on self.coordinates, ids are stored in self.pano_ids. 
-        Warning: 
-            Always call self.generate_coordinates prior to calling this method
-        """
-        i = 0
-        self.download_descriptor = len(self.pano_ids)
-        while i < len(self.coordinates):
-            lat, lon = self.coordinates[i]
-            panos = streetview.search_panoramas(lat, lon)
-            if len(panos) > 0:
-                self.pano_ids.append(panos[0].pano_id)
-            else:
-                self.coordinates.pop(i)
-                print(f'No panorama found at {lat}, {lon}')
-        if (len(self.pano_ids) < self.dataset_size):
-            print(f'Sampled {len(self.pano_ids)} samples. Some coordinates do not have a corresponding panorama')
-            self.dataset_size = len(self.pano_ids)
+            self.sample_size = len(self.coordinates)
             
     def download_street_view_images(self, images_dir:str) -> None:
         """Downloads street view images. 
@@ -106,9 +95,10 @@ class StreetViewSampler:
             images_dir (str): directory to save images to
         """
         os.makedirs(images_dir, exist_ok=True)
+        coord_counter = 0
         for i in range(self.download_descriptor, len(self.pano_ids)):
             pano_id = self.pano_ids[i]
-            lat, lon = self.coordinates[i]
+            lat, lon = self.coordinates[coord_counter]
             image_data = []
             
             for heading in constants.DEFAULT_HEADINGS:
@@ -132,6 +122,7 @@ class StreetViewSampler:
             
             image_name = f"{images_dir}/panorama_{lat}_{lon}.jpg"
             pano.save(image_name)
+            coord_counter += 1
         
     def save_sampler_status_metadata(self, metadata_dir:str, metadata_file_name:str=constants.DEFAULT_METADATA_FILE_NAME) -> None:
         """Saves sampler status metadata to a json file
@@ -145,19 +136,19 @@ class StreetViewSampler:
             file.write(json.dumps(self, default=StreetViewSampler.json_encoder))
             
     @staticmethod
-    def load_sampler_status_metadata(metadata_dir:str, dataset_size:int, api_key:str, metadata_file_name:str=constants.DEFAULT_METADATA_FILE_NAME) -> 'StreetViewSampler':
+    def load_sampler_status_metadata(metadata_dir:str, sample_size:int, api_key:str, metadata_file_name:str=constants.DEFAULT_METADATA_FILE_NAME) -> 'StreetViewSampler':
         """Loads sampler status metadata from json file
 
         Args:
             metadata_dir (str): directory to metadata file
-            dataset_size (int): size of dataset desired
+            sample_size (int): size of samples of current session
             api_key (str): your Google API key
             metadata_file_name (str, optional): name of metadata file. Defaults to constants.DEFAULT_METADATA_FILE_NAME.
         """
         jo = None
         with open(f'{metadata_dir}/{metadata_file_name}') as file:
             jo = json.load(file)
-        product = StreetViewSampler(dataset_size, api_key)
+        product = StreetViewSampler(sample_size, api_key)
         product.fov = jo['fov']
         product.image_size = jo['image_size']
         product.prompt_descriptor = jo['prompt_descriptor']
@@ -174,7 +165,7 @@ class StreetViewSampler:
             "pano_ids": obj.pano_ids,
             "image_size": obj.image_size,
             "fov": obj.fov,
-            "dataset_size": obj.dataset_size,
+            "sample_size": obj.sample_size,
             "prompt_descriptor": obj.prompt_descriptor,
             "image_width": obj.image_width,
             "image_height": obj.image_height
